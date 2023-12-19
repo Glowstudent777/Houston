a99cca91bc341695eed3564a75e9211b4b9edf88
Glowstudent777 <cameron@themenkens.com>
2023-12-18 21:52:45 -0600

    Initial Commit

---
+++ Houston.Bot/Program.cs
@@ -0,0 +1,28 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.Interactions;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using Houston.Bot.Services;
+
+namespace Houston.Bot;
+
+public class Program
+{
+	private static async Task Main(string[] args)
+	{
+		var services = ConfigureServices();
+		var bot = services.GetRequiredService<BotService>();
+
+		await bot.Initialize();
+
+		await Task.Delay(-1);
+	}
+
+	private static ServiceProvider ConfigureServices()
+	{
+		return new ServiceCollection()
+			.AddSingleton(new BotService())
+			.BuildServiceProvider();
+	}
+}
---
+++ Houston.Bot/Services/BotService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.Interactions;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Houston.Bot.Services;
+
+public class BotService : DiscordShardedClient
+{
+	private DiscordShardedClient _client;
+
+	public BotService()
+	{
+		_client = new DiscordShardedClient(new DiscordSocketConfig()
+		{
+			GatewayIntents = GatewayIntents.All,
+		});
+	}
+
+	public async Task Initialize()
+	{
+		_client.Log += LogAsync;
+
+		await _client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("TOKEN"));
+		await _client.SetGameAsync("NHL Games", null, ActivityType.Watching);
+
+		await _client.StartAsync();
+
+		_client.ShardReady += OnShardReady;
+	}
+
+	private async Task OnShardReady(DiscordSocketClient client)
+	{
+		if (client.ShardId != 0) return;
+		Console.WriteLine($"Logged in as {client.CurrentUser.Username}({client.CurrentUser.Id})");
+	}
+
+	private static Task LogAsync(LogMessage log)
+	{
+		if (log.Exception is GatewayReconnectException)
+		{
+			if (log.Exception.InnerException is not null)
+				Console.WriteLine(log.Exception.InnerException.Message);
+			return Task.CompletedTask;
+		}
+
+		Console.WriteLine(log.ToString());
+		return Task.CompletedTask;
+	}
+}
